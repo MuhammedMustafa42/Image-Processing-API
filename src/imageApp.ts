1b94782728913ce24f06525a6e9ded7a432ea8e0
MuhammedMustafa42 <100053538+MuhammedMustafa42@users.noreply.github.com>
2022-05-24 10:19:03 +0200

    Update imageApp.ts

--- src/imageApp.ts
+++ src/imageApp.ts
@@ -2,9 +2,12 @@ import sharp from 'sharp';
 import path from 'path';
 import { response } from 'express';
 
+//creates the path for the images before proccessing it 
 const imgPath = path.resolve('./images/full');
+//creates the path for the processed images
 const resizedImgPath = path.resolve('./images/resized');
 
+//the function that proccess the images using sharp
 const processImg = async (
   imgName: string,
   width: number,
